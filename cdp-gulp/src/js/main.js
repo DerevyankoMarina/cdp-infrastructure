f98009d4f9d94c86ed58a614aba7125beb466f4b
Maryna Derevianko <Maryna.Derevianko@travisperkins.co.uk>
2016-05-05 18:40:42 +0300

    Task-gulp is finished. Begin task-lint

---
+++ cdp-gulp/src/js/main.js
@@ -0,0 +1,7 @@
+var capitalize = require('capitalize-first-letter-cdp');
+var btn = document.getElementById('btn-hello');
+
+btn.addEventListener('click', function(){
+  var testStr = 'Lorem ipsum dolor sit amet, ipsum choro aperiam ut vel, insolens consulatu ea est';
+  console.log( capitalize(testStr) );
+}, false);
